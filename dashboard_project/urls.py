221619aeee6febb567ad3b09c6fc7cb18beb055a
Pietro Osores <168022195+Maximoff19@users.noreply.github.com>
2026-01-29 18:58:16 -0500

    Update urls.py

--- dashboard_project/urls.py
+++ dashboard_project/urls.py
@@ -1,6 +1,10 @@
+from django.contrib import admin
 from django.urls import path
 from core.views import home_dashboard
+from core.debug_view import debug_session
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', home_dashboard, name='home'),
+    path('debug-session/', debug_session),
 ]
